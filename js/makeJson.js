c4f5265667ccd276382154e7b520694eb9bc9340
adel beitvashahi <adelbeit@indiana.edu>
2018-05-21 20:34:29 -0400

    working on embedding google maps

--- js/makeJson.js
+++ js/makeJson.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 var dir = require('node-dir');
 
+var direct = "./img/cars 2/prius/";
 
 // fs = dir.files("./img/cars/", function(err, files) {
 //   if (err) throw err;
@@ -10,25 +11,34 @@ var dir = require('node-dir');
 
 // console.log(fs);
 
-fs.open("./img/cars/prius/info.txt",'w', fetchInfo);
+// write to files
+// fs.open("./img/cars 2/prius/info.txt",'w', fetchInfo);
 
+// function fetchInfo(err,filedata) {
+//     if (err) throw err;
 
-
-function fetchInfo(err,filedata) {
-    if (err) throw err;
-
-    console.log(filedata);
-    var k = {'ok':23,'whatnow':44};
-    fs.appendFile(filedata, k, 'utf8', (err) => {
+//     // console.log(filedata);
+//     var k = {'ok':23,'whatnow':44};
+//     fs.appendFile(filedata, k, 'utf8', (err) => {
         
-        fs.close(filedata, (err) => {
-            if (err) throw err;
-        });
-        if (err) throw err;
-    });
-}
+//         fs.close(filedata, (err) => {
+//             if (err) throw err;
+//         });
+//         if (err) throw err;
+//     });
+// }
 
 // console.log(fs.readdirSync('c:'));
 
-var fpaths = dir.files("./img/cars/", {sync:true});
-console.log(fpaths);
+// var fpaths = dir.files("./img/cars 2/", {sync:true});
+// console.log(fpaths);
+
+
+// read and write
+
+///////// read
+var readme = fs.readFileSync(direct+"info.txt", "utf8");
+console.log(readme);
+
+
+///////// write
