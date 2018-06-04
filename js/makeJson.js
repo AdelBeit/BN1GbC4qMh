b3d4f594a4db95101a043bc3089ac908e8808b93
adel beitvashahi <adelbeit@indiana.edu>
2018-06-04 02:21:19 -0400

    getting close with the node stuff

--- js/makeJson.js
+++ js/makeJson.js
@@ -1,7 +1,55 @@
-const fs = require('fs');
+var fs = require('fs');
 var dir = require('node-dir');
 
-var direct = "./img/cars 2/prius/";
+// printing function
+var print = function(args){
+    console.log(args);
+}
+
+// are there args?
+if (process.argv.length <= 2){
+    console.log("Usage: " + __filename + " path/to");
+    process.exit(-1);
+}
+
+// get path from args
+var path = '.';
+path = process.argv[2];
+
+// get list of cars
+var carlist = fs.readdirSync(path);
+
+
+carlist = carlist.filter(e => !e.includes('.json'));
+
+print(carlist);
+// console.log(dirlist);
+
+for (i = 0; i < carlist.length; i++){
+    var p2 = path+carlist[i];
+    print(p2);
+    var files = dir.files(p2,{sync:true});
+    print(files);
+}
+
+
+
+// console.log('files');
+// console.log(files);
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// var dir = require('node-dir');
+
+// var direct = "./img/cars 2/prius/";
 
 // fs = dir.files("./img/cars/", function(err, files) {
 //   if (err) throw err;
@@ -17,15 +65,15 @@ var direct = "./img/cars 2/prius/";
 // function fetchInfo(err,filedata) {
 //     if (err) throw err;
 
-const fs = require('fs');
-var dir = require('node-dir');
-var direct = "./img/cars 2/";
+// const fs = require('fs');
+// var dir = require('node-dir');
+// var direct = "./img/cars 2/";
 
 //////// get list of files 
 
-fs.readdirSync(direct).forEach(file => {
-    console.log(file);
-})
+// fs.readdirSync(direct).forEach(file => {
+//     console.log(file);
+// })
 
 
 // // ///////// read
--- js/test.js
+++ js/test.js
@@ -1,5 +1,120 @@
+///// list dir content 2 //////////////////////////
+var dir = require('node-dir');
+var path = '.';
+path = process.argv[2];
+// dir.files(__dirname, function(err, files) {
+// dir.files(path, function(err, files) {
+//     if (err) throw err;
+//     console.log(files);
+    
+//     for (var i=0; i<files.length; i++){
+//         console.log(files[i].slice(2,files[i].length));
+//     }
+//     // console.log(files);
+// });
 
-// read file async
+var files = dir.files(path,{sync:true});
+
+console.log('files');
+console.log(files);
+
+
+///// list dir cont //////////////////////////
+var fs = require('fs');
+
+if (process.argv.length <= 2){
+    console.log("Usage: " + __filename + " path/to");
+    process.exit(-1);
+}
+
+var path = process.argv[2];
+
+// fs.readdir(path, function(err,items){
+//     // console.log(items);
+
+//     for (var i=0; i<items.length; i++){
+
+//         var file = path + '/' + items[i];
+
+//         // console.log("start: " + file);
+
+//         // fs.stat(file, generate_callback(file));
+//         // console.log(items[i].stats);
+//     }
+//     return items;
+// });
+
+
+
+// function generate_callback(file){
+//     return function(err, stats){
+//         console.log(file);
+//     }
+// }
+
+var dirlist = fs.readdirSync(path);
+
+console.log(dirlist);
+
+
+///// drive/file info //////////////////////////
+// var fs = require('fs');
+
+// if (process.argv.length <= 2){
+//     console.log("Usage: " + __filename + " path/to");
+//     process.exit(-1);
+// }
+
+// var path = process.argv[2];
+
+// fs.stat(path, function(err, stats){
+//     console.log(path);
+//     console.log();
+//     console.log(stats);
+//     console.log();
+
+//     if (stats.isFile()){
+//         console.log('    file');
+//     }
+//     if (stats.isDirectory()){
+//         console.log('    directory');
+//     }
+
+//     console.log('    size: ' + stats["size"]);
+//     console.log('    mode: ' + stats["mode"]);
+//     console.log('    others eXecute: ' + (stats["mode"] & 1 ? 'x' : '-'));
+//     console.log('    others Write:   ' + (stats["mode"] & 2 ? 'w' : '-'));
+//     console.log('    others Read:    ' + (stats["mode"] & 4 ? 'r' : '-'));
+ 
+//     console.log('    group eXecute:  ' + (stats["mode"] & 10 ? 'x' : '-'));
+//     console.log('    group Write:    ' + (stats["mode"] & 20 ? 'w' : '-'));
+//     console.log('    group Read:     ' + (stats["mode"] & 40 ? 'r' : '-'));
+ 
+//     console.log('    owner eXecute:  ' + (stats["mode"] & 100 ? 'x' : '-'));
+//     console.log('    owner Write:    ' + (stats["mode"] & 200 ? 'w' : '-'));
+//     console.log('    owner Read:     ' + (stats["mode"] & 400 ? 'r' : '-'));
+ 
+ 
+//     console.log('    file:           ' + (stats["mode"] & 0100000 ? 'f' : '-'));
+//     console.log('    directory:      ' + (stats["mode"] & 0040000 ? 'd' : '-'));
+// })
+
+///// exitting code //////////////////////////
+// process.exit(-1);
+
+///// command line args //////////////////////////
+// console.log(process.argv);
+
+// if (process.argv.length <= 2){
+//     console.log("usage: " + __filename + " SOME_PARAM");
+//     process.exit(-1);
+// }
+
+// var param = process.argv[2];
+
+// console.log('param: ' + param);
+
+///// read file async //////////////////////////
 // var fs = require('fs');
 
 // fs.readFile("DATA", 'utf8', function(err, contents){
@@ -8,14 +123,14 @@
 
 // console.log('after calling readFile');
 
-// read file sync
-var fs = require('fs');
+///// read file sync //////////////////////////
+// var fs = require('fs');
 
-var contents = fs.readFileSync('DATA', 'utf8');
-console.log(contents);
-console.log('after calling readFile');
+// var contents = fs.readFileSync('DATA', 'utf8');
+// console.log(contents);
+// console.log('after calling readFile');
 
-// web server
+///// web server //////////////////////////
 // var http = require('http');
 // var port = 8080;
 
