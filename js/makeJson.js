b5e2e312525788c5824d5a74ade5575eb7d9dca4
adel beitvashahi <adelbeit@indiana.edu>
2018-06-05 02:56:44 -0400

    I WORK SO HAAAD

--- js/makeJson.js
+++ js/makeJson.js
@@ -19,18 +19,46 @@ path = process.argv[2];
 // get list of cars
 var carlist = fs.readdirSync(path);
 
-
 carlist = carlist.filter(e => !e.includes('.json'));
 
 print(carlist);
 // console.log(dirlist);
 
+var cardict = [];
+
 for (i = 0; i < carlist.length; i++){
-    var p2 = path+carlist[i];
-    print(p2);
+    var car = carlist[i];
+    var p2 = path+car;
+    // print(p2);
     var files = dir.files(p2,{sync:true});
-    print(files);
+
+    files = files.map(s => s = s.substring(path.length-1,s.length));
+
+    // print(files);
+
+    cardict.push({
+        "Image": car,
+        "OtherImages":files,
+        "Year": "2006",
+        "Make": "Toyota",
+        "Model": "Prius",
+        "Milage": "135,000",
+        "Transmission": "Auto",
+        "Cylinders": "4",
+        "Price": "6750"
+    });
+
+    print(cardict);
+
 }
+
+// for (i = 0; i < files.length; i++){
+//     var file = files[i];
+//     file = file.substring(2,file.length);
+//     files[i] = file;
+//     print(file);
+// }
+
 
 
 
